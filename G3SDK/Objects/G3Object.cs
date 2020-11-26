1e2662a0154c02af392f0ae2dfc74dbaa24156ae
Jonas Högström <jonas.hogstrom@tobii.com>
2020-11-26 11:27:13 +0100

    Added .net 5 target for the sdk assembly (and nuget package)

--- G3SDK/Objects/G3Object.cs
+++ G3SDK/Objects/G3Object.cs
@@ -2,9 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
